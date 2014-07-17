6ad10cd3b55948e6293cbd14e51694c2afe02cfb
Aaron Bassett <me@aaronbassett.com>
2014-07-18 00:38:00 +0100

    Create basic working version

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-import os
-from flask import Flask
+import random
+
+import yaml
+
+from flask import Flask, jsonify, request, Response, render_template
+from flask.ext.classy import FlaskView
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+class AppView(FlaskView):
+    route_base = '/'
+
+    def _accepts(self, mime):
+        best = request.accept_mimetypes.best_match([mime, 'text/html'])
+        return best == mime and\
+               request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+    @property
+    def _excuse(self):
+        stream = open("excuses.yaml", 'r')
+        excuses = yaml.load(stream)
+        return random.choice(excuses["excuses"])
+
+    def index(self):
+
+        if self._accepts("application/json"):
+            return jsonify({
+                "excuse": self._excuse
+            })
+        elif self._accepts("application/xml"):
+            return Response(
+                render_template('xml.xml', excuse=self._excuse),
+                mimetype='text/xml'
+            )
+        elif self._accepts("text/plain"):
+            return Response("Hello world", mimetype='text/plain')
+        else:
+            return render_template('html.html', excuse=self._excuse)
+
+AppView.register(app)
+
+if __name__ == '__main__':
+    app.run()
